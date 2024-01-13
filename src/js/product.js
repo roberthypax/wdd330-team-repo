3090c617cd0a9dfa910c06c345e916e3dd518642
MiguelSecadesBYU <sec21002@byui.edu>
2024-01-13 23:01:11 +0100

    Fix add to car broken button

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,19 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get the current cart from local storage
+  let cart = getLocalStorage("so-cart");
+
+  // If the cart does not exist or is not an array, create a new array
+  if (!cart || !Array.isArray(cart)) {
+    cart = [];
+  }
+
+  // Add new product to cart
+  cart.push(product);
+
+  // Saves the updated cart to local storage
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
