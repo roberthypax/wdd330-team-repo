3a265bd1abea487d805ef60535c513066bf35415
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2024-01-21 01:45:11 +0200

    fixed addProductToCart()

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,24 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get current cart array
+  let cart = JSON.parse(localStorage.getItem("so-cart")) || [];
+  // Check if product already in cart
+  let existingItem = cart.find(item => {
+    return item.Id === product.Id; 
+  });
+
+  if (existingItem) {
+    // Increment quantity of existing item
+    existingItem.quantity++;
+  } else {
+    // Add new product to cart 
+    product.quantity = 1;
+    cart.push(product);
+  }
+  // Save updated cart array
+  //localStorage.setItem("so-cart", JSON.stringify(cart));
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
